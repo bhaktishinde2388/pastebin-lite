746715c0730bd9337ed122b5e739c131912a587e
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-30 18:54:12 +0530

    Implement API to create and store a paste

--- server/routes/pasteRoutes.js
+++ server/routes/pasteRoutes.js
@@ -14,4 +14,47 @@ router.get("/healthz", async (req, res) => {
   }
 });
 
+// ......paste api
+router.post("/pastes", async (req, res) => {
+  try {
+    const { content, ttl_seconds, max_views } = req.body;
+
+    if (!content || content.trim() === "") {
+      return res.status(400).json({ error: "Content is required" });
+    }
+
+    if (ttl_seconds && ttl_seconds < 1) {
+      return res.status(400).json({ error: "Invalid TTL" });
+    }
+
+    if (max_views && max_views < 1) {
+      return res.status(400).json({ error: "Invalid max views" });
+    }
+
+    const pasteId = nanoid(8);
+    const now = new Date();
+
+    const expiresAt = ttl_seconds
+      ? new Date(now.getTime() + ttl_seconds * 1000)
+      : null;
+
+    const paste = new Paste({
+      pasteId,
+      content,
+      expiresAt,
+      remainingViews: max_views || null,
+    });
+
+    await paste.save();
+
+    res.status(201).json({
+      id: pasteId,
+      url: `http://localhost:5173/p/${pasteId}`,
+    });
+  } catch {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+
 export default router;
