fa51669cabc191bb0518bf4858590b841778b460
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-31 13:12:15 +0530

    fix view page render

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import "./Home.css"
 
 export default function Home() {
   const [content, setContent] = useState("");
--- client/src/views/ViewPaste.jsx
+++ client/src/views/ViewPaste.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./ViewPaste.css"
 
 export default function ViewPaste() {
   const { id } = useParams();
